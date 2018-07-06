75955f49b0572b0ee13da4c79ff59c385fff8a1a
yukito ohira <yukito.sys.so@gmail.com>
2018-07-07 00:03:26 +0900

    dotenv導入

--- db/migrate/20180706140804_create_items.rb
+++ db/migrate/20180706140804_create_items.rb
@@ -2,7 +2,9 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name, null: false
+      t.text :description, null: false, default: ''
       t.integer :price, null: false
+      t.integer :status, null: false
       t.timestamps
     end
   end
