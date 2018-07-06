4a25b742b7b934ed17e3a3ba5fdf4ae51c570f2f
yukito ohira <yukito.sys.so@gmail.com>
2018-07-06 17:55:06 +0900

    user migration

---
+++ db/migrate/20180706085407_create_user.rb
@@ -0,0 +1,9 @@
+class CreateUser < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :crypted_password
+      t.string :salt
+    end
+  end
+end
