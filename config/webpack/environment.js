6f2ae82251ad220f411c0acffa642d2fe9eb1bc2
yukito ohira <yukito.sys.so@gmail.com>
2018-07-07 00:52:24 +0900

    jqueryを読み込めるように修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,19 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
+const webpack = require('webpack')
 
+// vue
 environment.loaders.append('vue', vue)
+
+// jquery
+environment.plugins.prepend(
+  'Provide',
+  // bootstrap付属品
+  new webpack.ProvidePlugin({
+    $: 'jquery/dist/jquery',
+    jQuery: 'jquery/dist/jquery',
+    Popper: 'popper.js/dist/popper'
+  })
+)
+
 module.exports = environment
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,67 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_06_085407) do
+ActiveRecord::Schema.define(version: 2018_07_06_151031) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
-    t.string "email"
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "item_purchase_logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "seller_user_id", null: false
+    t.bigint "buyer_user_id", null: false
+    t.bigint "item_id"
+    t.integer "price", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_item_purchase_logs_on_item_id"
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.integer "price", null: false
+    t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reactions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "target_id", null: false
+    t.string "target_type", null: false
+    t.string "stamp_code", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reactions_on_user_id"
+  end
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_reviews_on_item_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
+    t.string "access_token"
+    t.string "access_token_secret"
+    t.string "screen_name", null: false
+    t.string "name"
+    t.text "description"
   end
 
 end
