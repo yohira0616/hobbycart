2701c6b4ccc174fd48b05f97ae4b11768fed16a0
yukito ohira <yukito.sys.so@gmail.com>
2018-07-07 00:58:19 +0900

    bootstrapを採用する男

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+require('bootstrap/dist/css/bootstrap.min.css')
+require('bootstrap/dist/js/bootstrap')
+
 console.log('Hello World from Webpacker')
