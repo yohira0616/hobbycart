1e39f6a3cf081b91d3c4c2b702fb5bcc3768dc85
yukito ohira <yukito.sys.so@gmail.com>
2018-07-11 22:43:39 +0900

    コメントのAPI部分を実装

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,19 +1,24 @@
 module Api
   class CommentsController < ::ApplicationController
-    # TODO: ログイン判定
+    before_action :require_login
 
     def create
+      Comment.create!(
+        user: current_user,
+        body: create_params[:body]
+      )
       render json: {}, status: 200
     end
 
     def destroy
+      Comment.destroy!(params[:id])
       render json: {}, status: 200
     end
 
     private
 
     def create_params
-
+      params.require(:comment).permit(:body)
     end
   end
 end
