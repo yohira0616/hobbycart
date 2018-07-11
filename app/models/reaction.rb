2887aea384e4814061eef414399bb77366b31c43
yukito ohira <yukito.sys.so@gmail.com>
2018-07-11 16:26:48 +0900

    リアクションvalidation追加

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,4 +1,9 @@
 # いいね！などのリアクションをつける
 class Reaction < ApplicationRecord
   belongs_to :user
+
+  validates :target_id, presence: true
+  validates :target_type, presence: true
+  validates :stamp_code, presence: true
+
 end
