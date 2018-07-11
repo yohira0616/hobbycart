f79bd2783040e9e64dab70b9b524f742b49f1c6d
yukito ohira <yukito.sys.so@gmail.com>
2018-07-11 17:01:05 +0900

    item_card作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   def index
     @q = Item.ransack(params[:q])
-    @items = @q.result.page(params[:page])
+    @items = @q.result.publish.page(params[:page])
   end
 
   def show
