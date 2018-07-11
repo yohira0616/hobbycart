4a37cf831533ac580ad36fc2446ddbfb2f58bf21
yukito ohira <yukito.sys.so@gmail.com>
2018-07-11 17:40:26 +0900

    いいねボタン基盤

---
+++ app/javascript/components/GoodButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="btn btn-light" @click="onClick"> いいね！</button>
+
+</template>
+
+<script>
+  export default {
+    name: "GoodButton",
+    methods: {
+      onClick() {
+        console.log('good!')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- app/javascript/packs/items/index.js
+++ app/javascript/packs/items/index.js
@@ -1,1 +1,5 @@
+import Vue from 'vue'
+import ReviewForm from '../../components/ReviewForm'
+import GoodButton from '../../components/GoodButton'
+
 console.log('items/index hello!')
