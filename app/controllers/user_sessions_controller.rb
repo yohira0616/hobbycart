677087e350c35e61d101c8c1e049bf0b737e4493
yukito ohira <yukito.sys.so@gmail.com>
2018-07-11 11:54:07 +0900

    ログアウトできるようにした

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,4 @@
-class UserSessionController < ApplicationController
+class UserSessionsController < ApplicationController
 
   # ログイン
   def new; end
@@ -6,6 +6,8 @@ class UserSessionController < ApplicationController
   # セッション破棄
   def destroy
     logout
+    flash[:primary]="ログアウトしました"
+    redirect_to root_path
   end
 
 end
