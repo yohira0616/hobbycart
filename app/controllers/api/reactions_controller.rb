feee3739b06ae1a187c67459f50478329a83218b
yukito ohira <yukito.sys.so@gmail.com>
2018-07-11 22:41:21 +0900

    いいねロジックのコントローラ部分を雑に実装

--- app/controllers/api/reactions_controller.rb
+++ app/controllers/api/reactions_controller.rb
@@ -1,20 +1,23 @@
 module Api
   class ReactionsController < ::ApplicationController
-    # TODO: ログイン判定
+    before_action :require_login
 
     def create
-
+      item = Item.find!(create_params[:item_id])
+      Reaction.good_to_item!(item, current_user)
       render json: {}, status: 200
     end
 
     def destroy
+      item = Item.find!(create_params[:item_id])
+      Reaction.revoke_good_to_item!(item, current_user)
       render json: {}, status: 200
     end
 
     private
 
     def create_params
-
+      params.permit(:item_id)
     end
   end
 end
