c4162d101d53b89f2d8c0f1952a44838495531fd
yukito ohira <yukito.sys.so@gmail.com>
2018-07-15 19:17:32 +0900

    コメントフォームのReactComponent

--- app/javascript/components/CommentForm.vue
+++ app/javascript/components/CommentForm.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>This is Comment Form</div>
+  <form>
+    <div class="form-group">
+      <p>コメント</p>
+      <textarea class="form-control" rows="3" placeholder="この商品に対してコメントする..." v-model="body" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-success" @click="onSubmit">コメントする</button>
+  </form>
 </template>
 
 <script>
+  import axios from '../modules/axios'
+
   export default {
-    name: "CommentForm"
+    name: "CommentForm",
+    data: function () {
+      return {
+        body: ''
+      }
+    },
+    methods: {
+      onSubmit(e) {
+        e.preventDefault()
+
+        console.log('submit')
+      }
+
+    }
   }
 </script>
 
