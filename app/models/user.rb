c99f05050a5ed46eb29b012d86d1670a16ef9672
yukito ohira <yukito.sys.so@gmail.com>
2018-07-16 19:59:47 +0900

    コメント欄にサムネイルを割り当てられるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery! do |config|
     config.authentications_class = Authentication
   end
+  has_one_attached :avatar
 
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
