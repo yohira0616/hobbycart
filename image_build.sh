47d2d346bba138762cf4ead19899dbcb3a689d62
yukito ohira <yukito.sys.so@gmail.com>
2018-07-25 17:58:45 +0900

    update dockerfile

--- image_build.sh
+++ image_build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # 本番用のイメージを固める
-docker build -t asia.gcr.io/compact-nirvana-140509/hobbycart-production:v4 -f Dockerfile.production .
+docker build -t asia.gcr.io/compact-nirvana-140509/hobbycart-production:v7 -f Dockerfile.production .
