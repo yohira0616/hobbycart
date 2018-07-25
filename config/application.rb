ed5fced388e4fc0a3a6dabe7d3a8ee6a5d4d16cd
yukito ohira <yukito.sys.so@gmail.com>
2018-07-25 15:47:44 +0900

    本番環境でdotenvを使わない処置

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+Dotenv::Railtie.load if %w(development test).include?(ENV['RAILS_ENV'])
 
 module Hobbycart
   class Application < Rails::Application
