03f8de77db5231319d07774820d41a6173a7d964
yukito ohira <yukito.sys.so@gmail.com>
2018-08-04 15:46:53 +0900

    ユーザーログイン情報のうち、jsに入れて問題ないものを入れるようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include AuthenticationHandler
+  before_action :set_user_information_to_gon
+
+
+  def set_user_information_to_gon
+    return unless logged_in?
+
+    gon.user_info = {
+
+    }
+  end
 
 end
