8056ed278365f906d31ed3315b5a397d8807b6a0
yukito ohira <yukito.sys.so@gmail.com>
2018-08-07 01:22:21 +0900

    ストレージに正しくアップロードできるように修正

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -2,9 +2,14 @@
 class Reaction < ApplicationRecord
   belongs_to :user
 
+  # これ商品id、本当は柔軟にしたかったけど
   validates :target_id, presence: true
   validates :target_type, presence: true
   validates :stamp_code, presence: true
+
+  def item
+    Item.find_by(id: target_id)
+  end
 
   class << self
     def already_good?(item, user)
